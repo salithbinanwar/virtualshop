26aa2071a6f80e32a1f93dbacbfdfa0ac274d22e
salith <salithanwar69@gmail.com>
2024-10-21 04:02:33 +0600

    netlify toml added

--- client/src/utils/axios.jsx
+++ client/src/utils/axios.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api/users',
+  // baseURL: 'http://localhost:5000/api/users',
+  baseURL: 'https://virtualshop-onlyserver.onrender.com/api/users',
   withCredentials: true,
 })
 export default axiosInstance
